994d299234e647f76702858057244f6e9943ee83
David <david.wang@mail.kway.com.tw>
2024-11-01 15:48:22 +0800

    加入專案檔案。

---
+++ WPFDemo_1734/Class/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDemo_1734.Class
+{
+    public class Stock
+    {
+        public string Stock_No { get; set; }
+        public string Stock_Name { get; set; }
+        public decimal Low_Price { get; set; }
+        public decimal High_Price { get; set; }
+        public DateTime Modify_Date { get; set; }
+        public string Modify_User { get; set; }
+    }
+}
---
+++ WPFDemo_1734/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using Dapper;
+using Oracle.ManagedDataAccess.Client;
+using WPFDemo_1734.Class;
+
+namespace WPFDemo_1734
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadStockData();
+        }
+
+        private void LoadStockData()
+        {
+            try
+            {
+                // 取得資料庫連線字串
+                string connectionString = ConfigurationManager.ConnectionStrings["OracleDbContext"].ConnectionString;
+
+                using (IDbConnection db = new OracleConnection(connectionString))
+                {
+                    // 查詢 Stock 表
+                    string query = "SELECT stock_no, stock_name, low_price, high_price, modify_date, modify_user FROM Stock";
+                    List<Stock> stockList = db.Query<Stock>(query).ToList();
+
+                    // 綁定資料到 DataGrid
+                    StockDataGrid.ItemsSource = stockList;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"資料載入時發生錯誤：{ex.Message}", "錯誤", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+   
+    
+}
