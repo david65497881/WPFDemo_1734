13bd59d0efa1bfcb74c1a7c380d7dbeabfd6b2ce
David <david.wang@mail.kway.com.tw>
2024-11-08 17:09:54 +0800

    新增註解

--- WPFDemo_1734/Class/Stock.cs
+++ WPFDemo_1734/Class/Stock.cs
@@ -11,11 +11,29 @@ namespace WPFDemo_1734.Class
     /// </summary>
     public class Stock
     {
+        /// <summary>
+        /// 股票代號
+        /// </summary>
         public string Stock_No { get; set; }
+        /// <summary>
+        /// 股票中文簡稱
+        /// </summary>
         public string Stock_Name { get; set; }
+        /// <summary>
+        /// 最低成交價
+        /// </summary>
         public decimal Low_Price { get; set; }
+        /// <summary>
+        /// 最高成交價
+        /// </summary>
         public decimal High_Price { get; set; }
+        /// <summary>
+        /// 異動日期
+        /// </summary>
         public DateTime Modify_Date { get; set; }
+        /// <summary>
+        /// 異動人員
+        /// </summary>
         public string Modify_User { get; set; }
     }
 }
